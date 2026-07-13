5a453f5afa40e0e6aecfba3a09afdfdc4d1fa81a
Javiera-Cuadra <javiera.cuadra.0@gmail.com>
2026-07-13 17:36:14 -0400

    Modificación leve de contenido de los textos en home.html, modificación animación de skill icons en .css y .js

--- script.js
+++ script.js
@@ -22,3 +22,35 @@ function toggleLogosOnScroll() {
 window.addEventListener('scroll', toggleLogosOnScroll);
 window.addEventListener('resize', toggleLogosOnScroll);
 document.addEventListener('DOMContentLoaded', toggleLogosOnScroll);
+
+// Selecciona todos los íconos de habilidades
+const icons = document.querySelectorAll('.skills_icon');
+
+icons.forEach(icon => {
+  let isMouseInside = false;
+
+  icon.addEventListener('mouseenter', () => {
+    isMouseInside = true;
+    icon.classList.remove('animate-down'); // Limpiamos por si acaso regresó rápido
+    icon.classList.add('animate-up');
+  });
+
+
+  icon.addEventListener('animationend', () => {
+    // Cuando termina de subir, revisamos: ¿El usuario ya quitó el cursor?
+    if (!isMouseInside) {
+      icon.classList.remove('animate-up');
+      icon.classList.add('animate-down');
+    }
+  });
+
+  // Cuando el cursor realmente se va, si ya terminó de subir, lo bajamos
+  icon.addEventListener('mouseleave', () => {
+    isMouseInside = false;
+    
+    // Si la animación de subida ya NO está activa (ya terminó), bajamos el ícono
+    if (!icon.classList.contains('animate-up')) {
+      icon.classList.add('animate-down');
+    }
+  });
+});
